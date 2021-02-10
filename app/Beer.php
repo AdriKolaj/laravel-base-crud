09d66e4512adca6bc8950d2d2b584897e2570f24
adri <adri.kolaj@gmail.com>
2021-02-10 17:18:13 +0100

    primo commit - stampa in pagina del db

---
+++ app/Beer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Beer extends Model
+{
+    protected $fillable = [
+        'marca',
+        'nome',
+        'gradazione',
+        'prezzo'
+    ];
+}
---
+++ database/migrations/2021_02_10_142641_create_beers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beers', function (Blueprint $table) {
+            $table->id();
+            $table->string('marca', 25);
+            $table->string('nome', 30);
+            $table->float('gradazione', 4, 2);
+            $table->float('prezzo', 5, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beers');
+    }
+}
